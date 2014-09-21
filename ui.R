947058d6dfda9347bab7e6eaa6d083f17c8f5b00
tgouhier <tarik.gouhier@gmail.com>
2014-09-21 12:23:30 -0400

    Update instructions

--- ui.R
+++ ui.R
@@ -8,9 +8,9 @@ shinyUI(pageWithSidebar(
     tags$head( tags$script(src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML-full", type = 'text/javascript'),
                tags$script( "MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});", type='text/x-mathjax-config')
     ),
-    p("The goal of this simulation is to demonstrate the trade-off between Type I error, the probability of rejecting the null hypothesis $H_0$ when it is true, and type II error, the probability of failing to reject the null hypothesis $H_0$ when it is false, under the Null Hypothesis Significance Testing framework."),
+    p("The goal of this simulation is to demonstrate the trade-off between Type I error $\\alpha$, the probability of rejecting the null hypothesis $H_0$ when it is true, and type II error $\\beta$, the probability of failing to reject the null hypothesis $H_0$ when it is false, under the Null Hypothesis Significance Testing framework."),
     
-p("Reducing type I error by decreasing $\\alpha$ will increase type II error and vice versa. You can verify the validity of this statement by using the controls below to alter the value of $\\alpha$."),
+p("Reducing type I error by decreasing $\\alpha$ will increase type II error and vice versa. You can verify the validity of this claim by using the controls below to alter the value of $\\alpha$."),
     h4("Simulation parameters:"),
     sliderInput("alpha", 
                 "Type I error ((\\alpha)):", 
